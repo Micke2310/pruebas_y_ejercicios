9a23cc64a2110bc9cb822e17f542bb031f5ca7eb
Micke2310 <maikyarachan@gmail.com>
2024-04-28 23:34:08 +0000

    ....

---
+++ logica-programacion/01-operadores-estructuras-control.c
@@ -0,0 +1,109 @@
+/* EN C*/
+
+/*Aritméticos:
++: Suma
+-: Resta
+*: Multiplicación
+/: División
+%: Módulo (resto de la división)
+
+Lógicos:
+&&: Y lógico (AND)
+||: O lógico (OR)
+!: Negación lógic(NOT)
+
+De comparación:
+==: Igual a
+!=: Diferente de
+<: Menor que
+>: Mayor que
+<=: Menor o igual que
+>=: Mayor o igual que
+
+Asignación:
+=: Asignación
++=: Suma y asignación
+-=: Resta y asignación
+*=: Multiplicación y asignación
+/=: División y asignación
+%=: Módulo y asignación
+<<=: Desplazamiento a la izquierda y asignación
+>>=: Desplazamiento a la derecha y asignación
+&=: AND a nivel de bits y asignación
+|=: OR a nivel de bits y asignación
+^=: XOR a nivel de bits y asignación
+
+Identidad:
+&: Devuelve la dirección de una variable
+*: Puntero a un tipo de dato
+
+Pertenencia:
+sizeof(): Devuelve el tamaño en bytes de un tipo de dato o una variable
+
+Bits:
+&: AND a nivel de bits
+|: OR a nivel de bits
+^: XOR a nivel de bits
+~: Complemento a uno (NOT a nivel de bits)
+<<: Desplazamiento a la izquierda
+>>: Desplazamiento a la derecha
+
+*/
+
+
+# PYTHON
+"""
+Aritméticos:
++: Suma
+-: Resta
+*: Multiplicación
+/: División
+%: Módulo (resto de la división)
+**: Potencia (exponenciación)
+
+Lógicos:
+and: Y lógico (AND)
+or: O lógico (OR)
+not: Negación lógic(NOT)
+
+De comparación:
+==: Igual a
+!=: Diferente de
+<: Menor que
+>: Mayor que
+<=: Menor o igual que
+>=: Mayor o igual que
+
+Asignación:
+=: Asignación
++=: Suma y asignación
+-=: Resta y asignación
+*=: Multiplicación y asignación
+/=: División y asignación
+%=: Módulo y asignación
+**=: Potencia y asignación
+//=: División entera y asignación
+&=: AND a nivel de bits y asignación
+|=: OR a nivel de bits y asignación
+^=: XOR a nivel de bits y asignación
+<<=: Desplazamiento a la izquierda y asignación
+>>=: Desplazamiento a la derecha y asignación
+
+Identidad:
+is: Devuelve True si dos variables son el mismo objeto
+is not: Devuelve True si dos variables no son el mismo objeto
+
+Pertenencia:
+in: Devuelve True si un valor está presente en una secuencia
+not in: Devuelve True si un valor no está presente en una secuencia
+
+Bits:
+&: AND a nivel de bits
+|: OR a nivel de bits
+^: XOR a nivel de bits
+~: Complemento a uno (NOT a nivel de bits)
+<<: Desplazamiento a la izquierda
+>>: Desplazamiento a la derecha
+"""
+
+
