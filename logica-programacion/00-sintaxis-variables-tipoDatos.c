4ee8cf6a79cc781bcc382a9efe6fd249bf6ef40a
Micke2310 <maikyarachan@gmail.com>
2024-04-28 21:40:37 +0000

    ....

---
+++ logica-programacion/00-sintaxis-variables-tipoDatos.c
@@ -0,0 +1,44 @@
+/** pagina oficial del 
+ * lenguaje que utilizamos
+ *
+ * https://devdocs.io/c 
+ * */
+
+// sintaxis de comentario en C
+
+/* sintaxis de comentario en formato admitido por Betty en C*/
+
+
+# pagina oficial en Phyton
+# https://python.org 
+# sntaxis de un comentario en Python
+
+"""
+esto tambien es un comentario
+en varias lineas en Python
+"""
+
+'''
+esto tambien
+es comentario
+en Python
+'''
+
+
+/*VARAIABLES*/
+
+
+char variable_general = "esto es una caja donde guardar datos en sus distintos tipos"
+
+char caracter; //  'A' caracteres en C
+bool buleano; // 'T'verdadero o 'F'falso en C
+	      
+float flotantesSimple; // numeros con coma 3.14f simple en C
+double flotantesDobles; // numeros con coma doble 3.141592653589793 en C
+long double flotanterxtendido; //numeros con coma extendidos 3.14159265358979323846L en C
+
+int enteros; // 10 numeros enteros en C
+short enteroCortos; // 1000 enteros cortos en C
+long enterosLargos; // 1000000L enteros largos en C
+long long enterosLargosLargos; // 1000000000LL enteros largos largos en C
+
